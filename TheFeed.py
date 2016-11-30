fbf8767c795189853dd4348422380db4da0680a4
taraokelly <taraokelly2@gmail.com>
2016-11-30 00:14:43 +0000

    Fifteenth Commit: Add settings.HTML and Logout Functionality

--- TheFeed.py
+++ TheFeed.py
@@ -8,7 +8,7 @@ import flask as fl
 
 app = fl.Flask(__name__)
 
-couch = couchdb.Server()
+#couch = couchdb.Server()
 
 couch = couchdb.Server('http://127.0.0.1:5984/')
 
@@ -16,20 +16,18 @@ db = couch['test1'] # existing
 
 db1 = couch['posts']
 
-loggedIn = 0
-
 cur_doc = ""
 
 @app.route("/")
 def root():
-	if(loggedIn==0):
+	if(not bool(cur_doc)):
 		return render_template("index.html", loggedOut="loggedOut")
 	rows = db1.view('_all_docs', include_docs=True)
 	docs = [row.doc for row in rows]
-	test = json.dumps((docs), indent=4)
-	tester = json.loads(test)
-	tester.reverse()
-	return render_template("index.html", home="home", cur_doc=cur_doc, posts=tester)
+	jstring = json.dumps((docs), indent=4)
+	posts = json.loads(jstring)
+	posts.reverse()
+	return render_template("index.html", home="home", cur_doc=cur_doc, posts=posts)
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -99,22 +97,34 @@ def addPost():
 	db1.save(doc)
 	return ''
 
+@app.route('/logout', methods=["GET", "POST"])
+def logout():
+	global cur_doc
+	cur_doc=""
+	return ''
+
 @app.route("/Error/<reason>")
 def error(reason):
-	if(loggedIn==0):
+	if(not bool(cur_doc)):
 		return render_template("index.html", loggedOut = "loggedOut", error=reason)
-	return render_template("index.html")
+	rows = db1.view('_all_docs', include_docs=True)
+	docs = [row.doc for row in rows]
+	jstring = json.dumps((docs), indent=4)
+	posts = json.loads(jstring)
+	posts.reverse()
+	return render_template("index.html", home="home", cur_doc=cur_doc, posts=posts)
 
 @app.route('/profile')
 def profile():
-	if(loggedIn==0):
+	if(not bool(cur_doc)):
 		return render_template("index.html", loggedOut = "loggedOut")
+	string=''
 	count = 0
 	for id in db1:
 		doc=db1[id]
 		if(doc['username']==cur_doc['username']):
 			if(count==0):
-				string = '['
+				string += '['
 			else:
 				string += ','
 			string += '{"username": "'+ doc['username'] +'", "post_content": "'+ doc['post_content'] +'", "post_time": "'+ doc['post_time'] +'"}'
@@ -125,7 +135,9 @@ def profile():
 	posts.reverse()
 	return render_template("index.html", profile="profile", cur_doc=cur_doc, posts=posts)
 
-#@app.route('/Home/logout')
+@app.route('/search')
+def search():
+	return ''
 
 if __name__ == "__main__":
    app.run()
