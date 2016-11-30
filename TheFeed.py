9d7b3c7f0ab8b3c9b74b0a0a3838e8008c405173
taraokelly <taraokelly2@gmail.com>
2016-11-30 17:42:23 +0000

    Sixteenth Commit: Add Delete Account Functionality

--- TheFeed.py
+++ TheFeed.py
@@ -1,19 +1,11 @@
-import couchdb
-import json
-import time
+import couchdb, json, time
 from flask import Flask, render_template, request
-from flask import request
-
 import flask as fl
 
 app = fl.Flask(__name__)
 
-#couch = couchdb.Server()
-
 couch = couchdb.Server('http://127.0.0.1:5984/')
-
 db = couch['test1'] # existing
-
 db1 = couch['posts']
 
 cur_doc = ""
@@ -28,6 +20,40 @@ def root():
 	posts = json.loads(jstring)
 	posts.reverse()
 	return render_template("index.html", home="home", cur_doc=cur_doc, posts=posts)
+
+@app.route("/Error/<reason>")
+def error(reason):
+	if(not bool(cur_doc)):
+		return render_template("index.html", loggedOut = "loggedOut", error=reason)
+	rows = db1.view('_all_docs', include_docs=True)
+	docs = [row.doc for row in rows]
+	jstring = json.dumps((docs), indent=4)
+	posts = json.loads(jstring)
+	posts.reverse()
+	return render_template("index.html", home="home", cur_doc=cur_doc, posts=posts)
+
+@app.route('/profile')
+def profile():
+	if(not bool(cur_doc)):
+		return render_template("index.html", loggedOut = "loggedOut")
+	posts=getPosts(cur_doc['username'])
+	return render_template("index.html", profile="profile", cur_doc=cur_doc, posts=posts)
+
+@app.route('/search')
+def search():
+	return ''
+
+@app.route('/settings')
+def settings():
+	if(not bool(cur_doc)):
+		return render_template("index.html", loggedOut = "loggedOut")
+	return render_template("index.html", settings="settings", cur_doc=cur_doc)
+
+@app.route('/logout', methods=["GET", "POST"])
+def logout():
+	global cur_doc
+	cur_doc=""
+	return ''
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -93,51 +119,51 @@ def addPost():
 		tags = json.loads(tags1)
 		doc = {'username' : cur_doc['username'], 'post_content' : post_content, 'post_time' : post_time, 'tags' : tags }
 	else:
-		doc = {'username' : cur_doc['username'], 'post_content' : post_content, 'post_time' : post_time}
+		doc = {'username' : cur_doc['username'], 'post_content' : post_content, 'post_time' : post_time }
 	db1.save(doc)
 	return ''
 
-@app.route('/logout', methods=["GET", "POST"])
-def logout():
-	global cur_doc
-	cur_doc=""
+@app.route('/delete', methods=["GET","POST"])
+def delete():
+	global cur_doc 
+	for id in db1:
+		doc = db1[id]
+		if(doc['username']==cur_doc['username']):
+			db1.delete(doc)
+	db.delete(cur_doc)
+	cur_doc= ""
 	return ''
 
-@app.route("/Error/<reason>")
-def error(reason):
-	if(not bool(cur_doc)):
-		return render_template("index.html", loggedOut = "loggedOut", error=reason)
-	rows = db1.view('_all_docs', include_docs=True)
-	docs = [row.doc for row in rows]
-	jstring = json.dumps((docs), indent=4)
-	posts = json.loads(jstring)
-	posts.reverse()
-	return render_template("index.html", home="home", cur_doc=cur_doc, posts=posts)
+@app.route('/update', methods=["GET","POST"])
+def update():
+	return ''
 
-@app.route('/profile')
-def profile():
-	if(not bool(cur_doc)):
-		return render_template("index.html", loggedOut = "loggedOut")
-	string=''
+@app.route('/securityCheck', methods=["GET","POST"])
+def check():
+	temp = fl.request.values["password"]
+	if(temp==cur_doc['password']):
+		return 'Correct'
+	return 'Incorrect Password'
+
+def getPosts(username):
+	posts=[]
 	count = 0
 	for id in db1:
 		doc=db1[id]
-		if(doc['username']==cur_doc['username']):
+		if(doc['username']==username):
 			if(count==0):
-				string += '['
+				string = '['
 			else:
 				string += ','
 			string += '{"username": "'+ doc['username'] +'", "post_content": "'+ doc['post_content'] +'", "post_time": "'+ doc['post_time'] +'"}'
 			count+=1
 	if(count!=0):
 		string+=']'
-	posts = json.loads(string)
-	posts.reverse()
-	return render_template("index.html", profile="profile", cur_doc=cur_doc, posts=posts)
-
-@app.route('/search')
-def search():
-	return ''
+		#Adapted from http://stackoverflow.com/questions/9347419/python-strip-with-n
+		string = string.replace('\n',' ')
+		posts = json.loads(string)
+		posts.reverse()
+	return posts
 
 if __name__ == "__main__":
    app.run()
